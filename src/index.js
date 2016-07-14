98269e35592c1869195d671da6046945d2a655f1
zengkai <kai.zeng@ele.me>
2016-07-14 14:13:32 +0800

    add react-dnd demo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+// import App from './components/Main';
+import Board from './components/board'
+import { observe } from './components/game'
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+observe(knightPositon => {
+  ReactDOM.render((
+    <Board knightPosition={knightPositon} />
+  ), document.getElementById('app'))
+})
